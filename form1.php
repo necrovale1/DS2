ea56d17727b90d880d222f68251bab6bb1502fba
necrovale1 <51262672+necrovale1@users.noreply.github.com>
2025-02-14 23:06:12 +0000

    a

--- form1.php
+++ form1.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <h1>Login de usuário</h1>
+        <form action="resultado1.php" method="GET">
+    <h3>Usuário :
+        <input type="text" name="usuario" required/></h3>
+    <h3>E-mail:
+        <input type="email" name="email" required/></h3>
+    <input typ="submit" name="enviar"></h3>
+</body>
+</html>
